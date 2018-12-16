784209f7ba1977d2b5eb8b4447977b8869f5823c
darkblackarts <darkblackarts@gmail.com>
2018-12-16 19:19:25 +0100

    Nowy model, zmeczenie, panika, wywalanie

--- test/Assets/FindWall.cs
+++ test/Assets/FindWall.cs
@@ -1,71 +1,171 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class FindWall : MonoBehaviour {
-
+	
+	float fatigue;
+	float rest;
+	bool zmeczyl;
 	// Use this for initialization
 	void Start () {
+        UnityEngine.AI.NavMeshAgent _navMeshAgent;
+        _navMeshAgent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+        _navMeshAgent.speed = Random.Range(3.0f, 6.0f);
 		
-	}
+		fatigue = Random.Range(2.0f, 4.0f)*1000000; //Zmeczenie
+		rest = Random.Range(1.0f, 2.0f)*1000000+fatigue; //Odpoczynek
+		zmeczyl = false; //Jeszcze sie nie zmeczyl
+		
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        FindClosestWall();
+        
+        NPCcode[] allAgents = GameObject.FindObjectsOfType<NPCcode>();
+        Debug.Log(allAgents[0]);
+        //var myVariable = myObject.GetComponent<NPCcode>().alreadyInPanic;
+        
+
+        UnityEngine.AI.NavMeshAgent _navMeshAgent;
+        _navMeshAgent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+		if(Time.time*1000000 > fatigue){
+			if(_navMeshAgent.speed > 1.0f && zmeczyl == false){
+				_navMeshAgent.speed = _navMeshAgent.speed-0.01f;
+				//Fetch the Renderer from the GameObject
+				//Renderer rend = GetComponent<Renderer>();
+
+				//Set the main Color of the Material to green
+				//rend.material.shader = Shader.Find("_Color");
+				//rend.material.SetColor("_Color", Color.blue);
+
+				//Find the Specular shader and change its Color to red
+				//rend.material.shader = Shader.Find("Specular");
+				//rend.material.SetColor("_SpecColor", Color.red);
+			}else{
+				zmeczyl = true;
+				if(_navMeshAgent.speed < 6.0f){
+					_navMeshAgent.speed = _navMeshAgent.speed+0.02f;
+					
+					//Fetch the Renderer from the GameObject
+					//Renderer rend = GetComponent<Renderer>();
+
+					//Set the main Color of the Material to green
+					//rend.material.shader = Shader.Find("_Color");
+					//rend.material.SetColor("_Color", Color.white);
+
+					//Find the Specular shader and change its Color to red
+					//  rend.material.shader = Shader.Find("Specular");
+					//rend.material.SetColor("_SpecColor", Color.red);
+				}
+			}
+		}
+
+        /*UnityEngine.AI.NavMeshHit hit;
+        UnityEngine.AI.NavMeshAgent _navMeshAgent;
+        _navMeshAgent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        if (UnityEngine.AI.NavMesh.FindClosestEdge(transform.position, out hit, 1))
+        {
+            double radius = (hit.distance);
+            if (radius > 2)
+            {
+                radius = 2;
+            }
+
+            float radiusRound = Mathf.Round((float)radius * 10.0f) * 0.1f;
+            _navMeshAgent.radius = radiusRound;
+
+            FindClosestAgents();
+
+            DrawCircle(transform.position, hit.distance, Color.red);
+            Debug.DrawRay(hit.position, Vector3.up, Color.red);
+        }*/
+        //FindClosestWall();
+    }
+	
+
+
+    void DrawCircle(Vector3 center, float radius, Color color)
+    {
+        Vector3 prevPos = center + new Vector3(radius, 0, 0);
+        for (int i = 0; i < 30; i++)
+        {
+            float angle = (float)(i + 1) / 30.0f * Mathf.PI * 2.0f;
+            Vector3 newPos = center + new Vector3(Mathf.Cos(angle) * radius, 0, Mathf.Sin(angle) * radius);
+            Debug.DrawLine(prevPos, newPos, color);
+            prevPos = newPos;
+        }
     }
 
-    void FindClosestWall()
+    void FindClosestAgents()
     {
-        int index = 0;
-        float distanceToClosestCube = Mathf.Infinity;
-        float distanceToClosestCubeTwo = Mathf.Infinity;
-        Cuber closestCube = null;
-        Cuber closestCubeTwo = null;
-        Cuber[] allCube = GameObject.FindObjectsOfType<Cuber>();
+        float distanceToClosestAgents = Mathf.Infinity;
+        NPCcode closestAgent = null;
+        NPCcode[] allAgents = GameObject.FindObjectsOfType<NPCcode>();
 
         UnityEngine.AI.NavMeshAgent _navMeshAgent;
         _navMeshAgent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
 
-
-        int i = 0;
-        foreach (Cuber currentCube in allCube)
+        //Znalezienie najblizszego obiektu
+        foreach (NPCcode currentAgent in allAgents)
         {
-            float distanceToCube = (currentCube.transform.position - this.transform.position).sqrMagnitude;
-            if (distanceToCube < distanceToClosestCube)
+            float distanceToAgent = (currentAgent.transform.position - this.transform.position).sqrMagnitude;
+            if (distanceToAgent < distanceToClosestAgents)
             {
-                distanceToClosestCube = distanceToCube;
-                closestCube = currentCube;
-                index = i;
+                distanceToClosestAgents = distanceToAgent;
+                closestAgent = currentAgent;
             }
-            i++;
         };
-        double radius = (distanceToClosestCube * 0.01)/1.5;
-        float radiusRound = Mathf.Round((float)radius * 10.0f) * 0.1f;
-        Debug.Log(radiusRound);
-        _navMeshAgent.radius = radiusRound;
+        NPCcode agentOne = closestAgent;
+        Debug.Log(distanceToClosestAgents);
+        /*Debug.Log("###########");
+       // Debug.Log(cubeOne.transform.position);
+
+        float sizeX = Mathf.Round((float)cubeOne.transform.localScale.x * 10.0f) * 0.1f;
+        float sizeZ = Mathf.Round((float)cubeOne.transform.localScale.z * 10.0f) * 0.1f;
+
+        float x = 0;
+        float z = 0;
+        float distanceToClosestEdge = Mathf.Infinity;
 
 
-        Cuber cubeOne = closestCube;
-        /*i = 0;
-        foreach (Cuber currentCube in allCube)
+
+
+        Vector3 clousestPossition = new Vector3();
+        while (x < sizeX/2)
         {
-            if (i != index)
+            z = 0;
+            while (z < sizeZ/2)
             {
-                float distanceToCube = (currentCube.transform.position - cubeOne.transform.position).sqrMagnitude;
-                if (distanceToCube < distanceToClosestCubeTwo)
+                Vector3 newPostition = new Vector3(cubeOne.transform.position[0] + x, cubeOne.transform.position[1], cubeOne.transform.position[2] + z);
+                float distanceToEdge = (newPostition - this.transform.position).sqrMagnitude;
+                if (distanceToEdge < distanceToClosestCube)
                 {
-                    distanceToClosestCubeTwo = distanceToCube;
-                    closestCubeTwo = currentCube;
+                    distanceToClosestEdge = distanceToEdge;
+                    clousestPossition = newPostition;
                 }
+
+                z = z + 0.1f;
             }
-            i++;
+            x = x + 0.1f;
+
         }
-        Debug.Log(distanceToClosestCubeTwo);
-        Cuber cubeTwo = closestCubeTwo;*/
+        Debug.Log(clousestPossition);
+        Debug.DrawLine(clousestPossition, this.transform.position);
+        */
+
+
+        //Zmiana radiusa
+        //double radius = (distanceToClosestCube * 0.01)/3;
+        //float radiusRound = Mathf.Round((float)radius * 10.0f) * 0.1f;
+        //_navMeshAgent.radius = radiusRound;
 
 
 
 
-        Debug.DrawLine(cubeOne.transform.position, this.transform.position);
+        //Debug.DrawLine(cubeOne.transform.position, this.transform.position);
     }
 }
--- test/Assets/NPCcode.cs
+++ test/Assets/NPCcode.cs
@@ -2,29 +2,71 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEditor;
 
 public class NPCcode : MonoBehaviour
 {
+    //public NavMeshSurface surface;
+
     [SerializeField]
     Transform _destination;
-
-    NavMeshAgent _navMeshAgent;
-
     
+    [SerializeField]
+    bool _enablePanic;
+    
+    [SerializeField]
+    bool _enableFalling;
+    
+    [SerializeField]
+    float wanderRadius = 0.5f;
 
+    [SerializeField]
+    float _panicProbability = 0.05f;
+    
+    [SerializeField]
+    float _panicTimeout = 0.5f;
+
+    [SerializeField]
+    float _fallProbability = 0.05f;
+    
+	float _updateInterval=0.1f;
+
+	bool destroyed=false;
+	bool alreadyInPanic = false;
+	bool alreadyFallen = false;
+	float panicTimer;
+
+    float fatigue;
+    float rest;
+    bool zmeczyl;
+
+
+    NavMeshBuildSettings _navMeshBuildSettings;
+	NavMeshAgent _navMeshAgent;
+	Material mat;
+	GameObject gobject;
+	
 	// Use this for initialization
 	void Start ()
     {
+		
         _navMeshAgent = this.GetComponent<NavMeshAgent>();
-
+	mat = GetComponent<Renderer>().material;
+	gobject = _navMeshAgent.gameObject;
         if (_navMeshAgent == null)
         {
-            Debug.LogError("Nav mesh not attached to " + gameObject.name);
+            //Debug.LogError("Nav mesh not attached to " + gameObject.name);
         }
         else
         {
+            _navMeshAgent.speed = Random.Range(30.0f, 60.0f); //Predkosc poczatkowa
+            fatigue = Random.Range(2.0f, 6.0f) * 1000000; //Zmeczenie
+            rest = Random.Range(1.0f, 2.0f) * 1000000 + fatigue; //Odpoczynek
+            zmeczyl = false; //Jeszcze sie nie zmeczyl
+
             SetDestination();
         }
+        StartCoroutine(updateCoroutine());
     }
 
     private void SetDestination()
@@ -33,11 +75,160 @@ public class NPCcode : MonoBehaviour
         {
             Vector3 targetVector = _destination.transform.position;
             _navMeshAgent.SetDestination(targetVector);
+
+
         }
     }
+    
+    private void SetRandomDestination()
+    {
+            Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+            _navMeshAgent.SetDestination(newPos);
+    }
+    
+        public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask) {
+		float randX = Random.Range(-dist, dist);
+		float randZ = Random.Range(-dist, dist);
+		if(randX>0.0f)
+			randX+=dist;
+		else
+			randX-=dist;
+		
+		if(randZ>0.0f)
+			randZ+=dist;
+		else
+			randZ-=dist;
+
+		Vector3 position = new Vector3(randX, 0, randZ);
+		//Debug.Log("x: "+position.x+"y: "+position.y+"z: "+position.z);	
+        position += origin; 
+        NavMeshHit navHit; 
+        NavMesh.SamplePosition (position, out navHit, dist, layermask);        
+        return navHit.position;
+    }
+    
+    void checkEscape()
+    {
+         if (Vector3.Distance (_destination.transform.position, transform.position) <= 50.0f)
+         {
+			destroyed=true;
+			Destroy(gameObject);
+		 }
+	}
+    
+    IEnumerator updateCoroutine()
+    {
+		while(true)
+		{
+			if(!destroyed)
+			{
+				checkEscape();
+				UpdateCustomPeriod();
+				yield return new WaitForSeconds(_updateInterval);
+			}
+			else
+			{
+				yield break;
+			}
+		}		
+	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+		 //// If the next update is reached
+         //if(Time.time>=nextUpdate){
+             ////Debug.Log(Time.time+">="+nextUpdate);
+             //// Change the next update (current second+1)
+             //nextUpdate=Mathf.FloorToInt(Time.time)+1;
+             //// Call your fonction
+             //UpdateCustomPeriod();
+         //}
+         //panicTimer+=Time.deltaTime;
 	}
+	
+	void UpdateCustomPeriod()
+	{
+        if (Time.time * 1000000 > fatigue)
+        {
+            if (_navMeshAgent.speed > 30.0f && zmeczyl == false)
+            {
+                _navMeshAgent.speed = _navMeshAgent.speed - 0.2f;
+                if (!alreadyInPanic && !alreadyFallen)
+                {
+                    mat.color = Color.blue;
+                }
+                else
+                {
+                    if (!alreadyFallen)
+                    {
+                        mat.color = Color.magenta;
+                    }
+                }
+                
+            }
+            else
+            {
+                zmeczyl = true;
+                if (_navMeshAgent.speed < 40.0f)
+                {
+                    _navMeshAgent.speed = _navMeshAgent.speed + 0.3f;
+                    if (!alreadyFallen && !alreadyInPanic)
+                    {
+                        mat.color = Color.white;
+                    }
+                    
+                }
+            }
+        }
+        if (_enablePanic)
+		{
+			panicTimer+=_updateInterval;
+			if(!alreadyInPanic && UnityEngine.Random.Range(0f,1f)<=_panicProbability && !alreadyFallen)
+			{
+				alreadyInPanic = true;
+				panicTimer = 0.0f;
+				//To something!		
+				//Debug.Log("PANIIIIIIIC");		
+				SetRandomDestination();
+				mat.color = Color.yellow;
+			
+			}
+			else
+			{
+				//Debug.Log("paniTimer: "+panicTimer+" _paniTimeout: "+_panicTimeout);
+				if(panicTimer>=_panicTimeout && !alreadyFallen)
+				{
+					//Debug.Log("I'm calm now...");	
+					alreadyInPanic=false;
+                    if (zmeczyl)
+                    {
+                        mat.color = Color.white;
+
+                    }
+					SetDestination();
+					panicTimer=0.0f;
+				}
+			}
+		}
+		if(_enableFalling)
+		{
+			if(!alreadyFallen && UnityEngine.Random.Range(0f,1f)<=_fallProbability)
+			{
+				alreadyFallen = true;
+				//To something!
+				_navMeshAgent.isStopped = true;
+				//_navMeshAgent.velocity = Vetor3.zero;
+				GameObjectUtility.SetNavMeshArea(gobject,3);		
+				//Debug.Log("I'm falling... I'm falling... I'm falling...");		
+				mat.color = Color.red;
+			}
+		}
+	//	surface.BuildNavMesh();	
+	}
+	
 }
+
+        
+
+
